ff9f67254691520bfbccf3ed2877f8322a642929
Jahanzeb Naeem <>
2023-10-12 18:37:29 +0500

    Localization
    ASP.NET 5 Localization with resource file.

---
+++ LocalizationSampleOld/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using LocalizationSampleOld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LocalizationSampleOld.Controllers
+{
+    public class HomeController : MyController
+    {
+        // GET: Home      
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Registration r)
+        {
+            return View(r);
+        }
+        public ActionResult ChangeLanguage(string lang)
+        {
+            new LanguageMang().SetLanguage(lang);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
